881ac67885fe364ddd7ff701920a7743a4cd262b
Lukluk <luklukaha@gmail.com>
2015-10-18 12:38:55 +0700

    remove BAD query

--- app/code/local/Mamoku/Multistockpoint/sql/multistockpoint_setup/mysql4-install-0.1.0.php
+++ app/code/local/Mamoku/Multistockpoint/sql/multistockpoint_setup/mysql4-install-0.1.0.php
@@ -2,17 +2,13 @@
 $installer = $this;
 $installer->startSetup();
 $sql=<<<SQLTEXT
-DROP TABLE IF EXISTS  anyuse;
- create table anyuse(id int not null auto_increment, name varchar(100), primary key(tablename_id));
-    insert into tablename values(1,'default');
-    
-		
+
+
 SQLTEXT;
 
 $installer->run($sql);
-//demo 
+//demo
 //Mage::getModel('core/url_rewrite')->setId(null);
-//demo 
+//demo
 echo "multi stock point";
 $installer->endSetup();
-	 
