4895d9d3f2201492bd6ba9dfd01c62283e23cd36
Lukluk <luklukaha@gmail.com>
2015-11-08 17:50:05 +0700

    updaete

--- app/code/local/Mamoku/Multistockpoint/Model/Product.php
+++ app/code/local/Mamoku/Multistockpoint/Model/Product.php
@@ -5,8 +5,18 @@ class Mamoku_Multistockpoint_Model_Product extends Mage_Catalog_Model_Product
     {
          if(Mage::getSingleton('customer/session')->isLoggedIn()){
              $customer=Mage::getSingleton('customer/session')->getCustomer();
-             //print
-             return parent::getPrice();
+             //print             
+             $obj=json_decode(parent::getPrice_qty(),true);             
+             if($obj['price']){
+             	if(intval($obj['price'][$customer->getStockpoint_id()])>0){
+             		return $obj['price'][$customer->getStockpoint_id()];
+             	}else{
+             		return parent::getPrice();
+             	}
+             }else{
+             	return parent::getPrice();
+             }
+             
          }else{
              return parent::getPrice();
          }
