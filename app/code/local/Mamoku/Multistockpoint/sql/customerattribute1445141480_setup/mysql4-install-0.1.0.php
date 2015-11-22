c4b808abef4b978abffda142b4b34074845bde66
Ali Sadikin <alisadikin008@gmail.com>
2015-11-22 15:50:40 +0700

    location_coverage tble

--- app/code/local/Mamoku/Multistockpoint/sql/customerattribute1445141480_setup/mysql4-install-0.1.0.php
+++ app/code/local/Mamoku/Multistockpoint/sql/customerattribute1445141480_setup/mysql4-install-0.1.0.php
@@ -236,16 +236,20 @@ $installer->addAttribute("customer", "outlet_owner",  array(
 
   ));
 $attribute   = Mage::getSingleton("eav/config")->getAttribute("customer", "outlet_owner");
-$used_in_forms = array();
+$used_in_forms=array();
+
 $used_in_forms[]="adminhtml_customer";
+$used_in_forms[]="checkout_register";
+$used_in_forms[]="customer_account_create";
+$used_in_forms[]="customer_account_edit";
 $used_in_forms[]="adminhtml_checkout";
         $attribute->setData("used_in_forms", $used_in_forms)
-    ->setData("is_used_for_customer_segment", true)
-    ->setData("is_system", 0)
-    ->setData("is_user_defined", 1)
-    ->setData("is_visible", 1)
-    ->setData("sort_order", 100)
-    ;
+        ->setData("is_used_for_customer_segment", true)
+        ->setData("is_system", 0)
+        ->setData("is_user_defined", 1)
+        ->setData("is_visible", 1)
+        ->setData("sort_order", 100)
+        ;
 $attribute->save();
 $installer->addAttribute("customer", "outlet_name",  array(
     "type"     => "varchar",
--- app/code/local/Mamoku/Multistockpoint/sql/multistockpoint_setup/mysql4-install-0.1.0.php
+++ app/code/local/Mamoku/Multistockpoint/sql/multistockpoint_setup/mysql4-install-0.1.0.php
@@ -3,46 +3,7 @@ $installer = $this;
 $installer->startSetup();
 $sql=<<<SQLTEXT
 
--- MySQL dump 10.13  Distrib 5.6.19, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: indonesia
--- ------------------------------------------------------
--- Server version	5.6.19-0ubuntu0.14.04.1-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-DROP TABLE IF EXISTS `districts`;
-
-DROP TABLE IF EXISTS `provinces`;
-
-DROP TABLE IF EXISTS `regencies`;
-
-DROP TABLE IF EXISTS `villages`;
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2015-08-17 23:25:03
-
-
-DROP TABLE IF EXISTS kelurahan,kecamatan,stockpoint;
+DROP TABLE IF EXISTS stockpoint;
 
 CREATE TABLE IF NOT EXISTS `stockpoint` (
  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -58,6 +19,17 @@ CREATE TABLE IF NOT EXISTS `stockpoint` (
  `description` varchar(100) DEFAULT NULL,
  `catatan` text,
  `last_sync_time` datetime
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS location_coverage;
+
+CREATE TABLE IF NOT EXISTS `location_coverage` (
+ `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ `propinsi` varchar(20) NOT NULL,
+ `kota` varchar(50) NOT NULL,
+ `kecamatan` varchar(50) NOT NULL,
+ `kelurahan` varchar(50) NOT NULL,
+ `stockpoint_code` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 SQLTEXT;
 
